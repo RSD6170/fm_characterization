5e8c35ae0798008c2140b602018d3a3488a1f651
Raphael Dunkel <rsd6170+github@mailbox.org>
2024-12-24 12:37:12 +0000

    feat: add fallback feature model reading

--- main_characterization.py
+++ main_characterization.py
@@ -10,22 +10,14 @@ from flamapy.metamodels.fm_metamodel.transformations import UVLReader, FeatureID
 from fm_characterization import FMCharacterization, PartwiseCharacterization
 
 
-def read_fm_file(filename: str) -> Optional[FeatureModel]:
+def read_fm_file(filenameUVL: str, filenameXML : str) -> Optional[FeatureModel]:
     try:
-        if filename.endswith(".uvl"):
-            return UVLReader(filename).transform()
-        elif filename.endswith(".xml") or filename.endswith(".fide"):
-            return FeatureIDEReader(filename).transform()
+        return UVLReader(filenameUVL).transform()
     except Exception as e:
         print(e)
         pass
     try:
-        return UVLReader(filename).transform()
-    except Exception as e:
-        print(e)
-        pass
-    try:
-        return FeatureIDEReader(filename).transform()
+        return FeatureIDEReader(filenameXML).transform()
     except Exception as e:
         print(e)
         pass
@@ -33,15 +25,15 @@ def read_fm_file(filename: str) -> Optional[FeatureModel]:
 
 
 
-def main(fm_filepath: str, mode : str) -> None:
+def main(path_UVL: str, path_XML: str, mode : str) -> None:
     path = pathlib.Path(fm_filepath)
     filename = path.stem
     dir = path.parent
 
     # Read the feature model
-    fm = read_fm_file(fm_filepath)
+    fm = read_fm_file(path_UVL, path_XML)
     if fm is None:
-        raise Exception('Feature model format not supported.')
+        raise Exception('Error in FM Parsing.')
     
     characterization = PartwiseCharacterization(fm, mode)
 
@@ -56,9 +48,10 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.ERROR)
     
     parser = argparse.ArgumentParser(description='FM Characterization.')
-    parser.add_argument(metavar='path', dest='path', type=str, help='Input feature model.')
+    parser.add_argument('pathUVL', type=str, help='Input feature model as UVL.')
+    parser.add_argument('pathXML', type=str, help='Input feature model as XML.')
     parser.add_argument("--mode", help="Subset to analyze, defaults to all", choices=["all", "metrics", "analysis_full", "analysis_light"], default="all")
 
     args = parser.parse_args()
 
-    main(args.path, args.mode)
+    main(args.pathUVL, args.pathXML, args.mode)
